a42d39374f9c10d7551268455d4fd8a9a69d30ca
zoubo9034 <52655911+zoubo9034@users.noreply.github.com>
2020-10-25 11:09:15 +0800

    Update env.py

--- light_rl/env.py
+++ light_rl/env.py
@@ -10,17 +10,18 @@ from utils import *
 
 
 class TrafficAgent():
-    def __init__(self):
-        
-        self.time0 = 0
+    def __init__(self):        
+        self.time0 = time.time()
         self.eff_main_cost = 2.0
-        self.eff_add_cost = 1
+        self.eff_add_cost = 0.5
         self.period_time = 80.0
         self.ser = serial.Serial("COM7", 19200, timeout=5)
         self.ser.parity = 'E'
-        self.embedding = nn.Embedding(2, 24)
-        self.embedding2 = nn.Embedding(2, 1) 
+        #self.embedding = nn.Embedding(2, 24)
+        self.embedding = embed48#nn.Embedding(2, 48)
+        self.embedding2 = embed1#nn.Embedding(2, 1) 
         self.light_state = np.array([1, 0])   # init light state
+        self.last_reward = 0
 
     def get_reward(self, action,add_flag=False):
         ### the main cost value
@@ -32,13 +33,37 @@ class TrafficAgent():
         fh.close()
         main_cost = float(main_cost)
         '''
+        
         _,vel_pos = self.get_vehicle_position()
         a, b = np.ones(48), np.zeros(48)
         v1 = np.concatenate((a,b,b,b,a,b,b,b),axis=0)
         v2 = np.concatenate((b,b,a,b,b,b,a,b),axis=0)
         c1 = np.float(np.dot(vel_pos.reshape(1,-1), v1))
         c2 = np.float(np.dot(vel_pos.reshape(1,-1), v2))
-        main_cost=0.7 * c1 + 0.3 * c2
+        #main_cost=0.7 * c1 + 0.3 * c2
+
+        if((self.light_state==[0,1]).all()):
+            if(action[0]==1):
+                main_cost=c1
+            if(action[1]==1):
+                main_cost=c2               
+        if((self.light_state==[1,0]).all()):
+            if(action[0]==1):
+                main_cost=c2
+            if(action[1]==1):
+                main_cost=c1  
+
+
+
+        '''
+        fh = open(REWARD_FILE, 'r') 
+        cost =fh.read()
+        while(cost == ''):
+            cost = fh.read()
+        fh.close()
+        main_cost = 0 - (float(cost)-self.last_reward)*10
+        self.last_reward=float(cost)
+        '''
 
         ### an additional cost value
         add_cost = 0
@@ -117,6 +142,7 @@ class TrafficAgent():
         return vel_pos,ori_pos
 
     def get_light_info(self):
+        '''
         light = self.traffic_light()
         if((light != self.light_state).all()):
             self.time0 = time.time()
@@ -129,23 +155,36 @@ class TrafficAgent():
 
         Node9_feat = torch.cat((N9FirstHalf, N9SecondHalf), dim=1)
         return Node9_feat
+        '''
+        light = self.traffic_light()
+        if((light != self.light_state).all()):
+            self.time0 = time.time()
+            self.light_state = light
+        light = self.light_state[0]
+        Node9_feat = self.embedding(torch.LongTensor([light]))  # light = 0 or 1
+        
+        longOflight = time.time() - self.time0
+        Node10_feat = torch.FloatTensor(np.ones([1, 48]) * longOflight / self.period_time)
+        
+        return Node9_feat, Node10_feat
+
 
     def send_action(self):
         begin = time.time()
         while ((time.time()-begin)<0.5):
             self.ser.write('1\0'.encode('gbk'))
-        time.sleep(0.2)
-
+        
     def get_state(self):
         vel_pos,_ = self.get_vehicle_position()
-        light_info = self.get_light_info()
-        state = torch.cat((torch.FloatTensor(vel_pos), light_info), dim=0)
+        light_info1, light_info2 = self.get_light_info()
+        state = torch.cat((torch.FloatTensor(vel_pos), light_info1, light_info2), dim=0)
         return state
 
     def step(self, action):
         reward = self.get_reward(action)
         if(action[0]==1):
-            self.send_action()       
+            self.send_action()
+        time.sleep(3)    
         next_state = self.get_state()        
 
         return reward, next_state
