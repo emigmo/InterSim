1b33ec48a5a1466d33780b98c303d3752394a418
zoubo9034 <52655911+zoubo9034@users.noreply.github.com>
2020-10-25 11:10:30 +0800

    Update utils.py

--- light_rl/utils.py
+++ light_rl/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import torch
+import torch.nn as nn
+'''
 ADJ_matrix = np.array(
               [[1,0,0,0,0,0,0,0,1],
               [0,1,0,0,0,0,0,0,1],
@@ -10,7 +12,22 @@ ADJ_matrix = np.array(
               [0,0,0,0,0,0,1,0,1],
               [0,0,0,0,0,0,0,1,1],
               [1,1,1,1,1,1,1,1,1]])
-
+'''
+ADJ_matrix = np.array(
+              [[1,0,0,0,0,0,0,0,1,1],
+              [0,1,0,0,0,0,0,0,1,1],
+              [0,0,1,0,0,0,0,0,1,1],
+              [0,0,0,1,0,0,0,0,1,1],
+              [0,0,0,0,1,0,0,0,1,1],
+              [0,0,0,0,0,1,0,0,1,1],
+              [0,0,0,0,0,0,1,0,1,1],
+              [0,0,0,0,0,0,0,1,1,1],
+              [1,1,1,1,1,1,1,1,1,1],
+              [1,1,1,1,1,1,1,1,1,1]])
 COST_FILE = 'D:\cost.txt'
 TRAFFIC_FILE = 'D:\TrafficLight.txt'
 VEL_POSTION_DIR = 'D:\DataRaw\DataSet'
+REWARD_FILE='D://reward.txt'
+
+embed1 = torch.load('embed1.pkl').eval()
+embed48 = torch.load('embed48.pkl').eval()
